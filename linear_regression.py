88cc90fb7226ce9e212ca8602fd1f9f6900791d5
Qonus <a1bat111k@mail.ru>
2025-03-19 16:33:23 +0500

    [Update]

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets import Slider, Button
+import random
 
 def calculate_loss(points, w, b) -> int:
     loss = 0
@@ -19,9 +20,13 @@ def calculate_slope(points, w, b) -> list[int]:
         db += (prediction - point[1]) * 2
     return [dw / M, db / M]
 
-points = np.array([(1, 4), (3, 4), (5, 5)])
+points = []
+for i in range(1, 10):
+    t = random.randrange(0, 10)
+    points.append((t, t + random.randrange(-5, 5)))
+points = np.array(points)
 baby_step = 0.01
-steps = 1000
+steps = 100
 w = 0
 b = 0
 
@@ -45,6 +50,8 @@ xloss = np.arange(1, steps)
 yloss = np.array(loss_graph)
 
 l1, = ax1.plot(xloss, loss_graph)
+ax1.set_xlabel('Generation')
+ax1.set_ylabel('Loss')
 
 xpoints = points[:, 0]
 ypoints = points[:, 1]
@@ -52,12 +59,15 @@ ypoints = points[:, 1]
 ax2.plot(xpoints, ypoints, "o")
 
 x = np.linspace(0, 10, 100)
-line, = ax2.plot(x, w_values[steps - 1] * x + b_values[steps - 1])
+line, = ax2.plot(x, w_values[0] * x + b_values[0])
 ax2.set_xlabel('X')
 ax2.set_ylabel('Y')
 
+# for i in range(len(xpoints)):
+#     plt.plot([xpoints[i], xpoints[i]], [ypoints[i], w_values[0] * points[i,0] + b_values[0]], color='gray', linestyle='--')
+
 axgen = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-gen = Slider(ax=axgen,label='Generation',valmin=1, valmax=steps - 900, valinit=1, valstep=1)
+gen = Slider(ax=axgen,label='Generation', valmin=0, valmax=steps - 1, valinit=0, valstep=1)
 
 def update_gen(val):
     val = int(val)
@@ -68,5 +78,12 @@ def update_gen(val):
     fig.canvas.draw_idle()
 
 gen.on_changed(update_gen)
+
+nextax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+button = Button(nextax, 'Next', hovercolor='0.975')
+
+def next_gen(event):
+    gen.set_val(gen.val + 1)
+button.on_clicked(next_gen)
 
 plt.show()
