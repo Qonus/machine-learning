3aad666020a2c264a99f0ff1c988c76591bc2654
Qonus <a1bat111k@mail.ru>
2025-03-19 08:39:56 +0500

    [Add] loss calculation

--- ml.py
+++ ml.py
@@ -1,27 +1,23 @@
-# import matplotlib
-# matplotlib.use('TkAgg')  # 'TkAgg', 'Qt5Agg', 'QtAgg', 'WXAgg'
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import numpy as np
-from matplotlib.widgets import Slider
 
-# Sample ML data (replace with your model's output)
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+points = np.array([(1, 4), (3, 4), (5, 5)])
 
-fig, ax = plt.subplots()
-line, = ax.plot(x, y)
+xpoints = points[:, 0]
+ypoints = points[:, 1]
 
-axcolor = 'lightgoldenrodyellow'
-ax_freq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-freq_slider = Slider(ax_freq, 'Frequency', 0.1, 30.0, valinit=1)
+def calculate_loss(points, w, b) -> int:
+    loss = 0
+    for point in points:
+        prediction = w * point[0] + b
+        loss += np.square(prediction - point[1])
+    return loss
 
-def update(val):
-    freq = freq_slider.val
-    new_y = np.sin(x * freq)
-    line.set_ydata(new_y)
-    fig.canvas.draw_idle()
+def calculate_slope(points, w, b) -> list[int]:
+    
+    return w
 
-freq_slider.on_changed(update)
-
+loss = calculate_loss(points, 0, 0)
+print(loss)
+plt.plot(xpoints, ypoints, "o")
 plt.show()
